c3881f87ffceff92f531906037a227bf816d647b
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-12-04 23:28:11 -0800

    debug code

--- Java/danger_zone/DangerControlUDP.java
+++ Java/danger_zone/DangerControlUDP.java
@@ -283,6 +283,7 @@ public class DangerControlUDP  extends DangerControl{
 		//Lets send the response as a json array of the nodes
 		String responseString = "{@}";
 		responseString = responseString.replace("@",neighbors.toString());
+		System.out.println(neighbors.toString() + neighbors.size()) ;
 		responseString = responseString.replace("[","");
 		responseString = responseString.replace("]","");
 		// Send reply.
